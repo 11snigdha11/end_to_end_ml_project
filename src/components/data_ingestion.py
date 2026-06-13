c2fcee737a98a44fb9bec7cd9fae9d26d1422d0e
11snigdha11 <snigdha.p2004@gmail.com>
2026-06-13 10:05:04 +0530

    model_trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -29,7 +29,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 # The @dataclass decorator automatically generates the __init__ method for us behind the scenes.
 @dataclass  
@@ -123,4 +124,6 @@ if __name__ == "__main__":
 
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
